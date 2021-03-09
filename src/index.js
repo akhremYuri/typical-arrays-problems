83d371876da86e9c26be91d560fdbe6b018ca5f4
akhremYuri <akhremr@gmail.com>
2021-03-09 13:03:06 +0300

    Add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minVal = 0;
+  if (array && array.length > 0) {
+    minVal = array[0];
+    for (let i = 0; i < array.length; i++){
+      if (minVal > array[i])
+        minVal = array[i];
+    }
+  }
+  return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxVal = 0;
+  if (array && array.length > 0) {
+    maxVal = array[0];
+    for(let i = 0; i < array.length; i++){
+      if (maxVal < array[i])
+        maxVal = array[i];
+    }
+  }
+  return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgVal = 0;
+  if (array && array.length > 0) {
+    for(let i = 0; i < array.length; i++)
+      avgVal += array[i];
+    avgVal = avgVal/array.length;
+  }
+  return avgVal;
 }
